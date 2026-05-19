a4ca84904517c86d64c1ec3c108773d4b16be408
unknown <hisun0217@gmail.com>
2026-05-19 10:16:26 +0900

    fix: disable SW caching

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dongbaek-admin-v1';
+const CACHE_NAME = 'dongbaek-admin-v2';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
@@ -7,12 +7,13 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
+// 캐싱 완전 비활성화 - 항상 네트워크에서 가져옴
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(fetch(event.request));
 });
